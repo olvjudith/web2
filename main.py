c7b81cef7a6eefd9898a4ac3c65234054e9c603e
olvjudith <olvjudith@gmail.com>
2024-01-02 19:51:57 -0600

    Start new project

---
+++ main.py
@@ -0,0 +1,23 @@
+from tools import sum
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    c = sum(1,2)
+
+
+    return f"<p style='color:red'>Hello, World!</p>"
+
+
+@app.route("/web_sum")
+def web_sum():
+    c = sum(1,2)
+    nums = list(range(100))
+
+    return f"<p>c= {c}</p>"
+
+
+
